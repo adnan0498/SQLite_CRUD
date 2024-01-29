2bff2f37c29d93eff8a9634d71f34154fe08517d
Adnan Ashraf <107193611+adnan0498@users.noreply.github.com>
2024-01-29 09:41:40 +0000

    Initial Commit

---
+++ SQLite CRUD/SQLite_CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLite_CRUDApp.swift
+//  SQLite CRUD
+//
+//  Created by Adnan on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SQLite_CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
